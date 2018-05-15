b5b8f4ca9974ea4c0b2b29e2d9ca9da3b80b3b44
Jorge Michele Hernandez Bucci <jhernandez@virtualrobotstud.io>
2018-05-15 15:03:24 -0500

    paginas generada login, main-menu, create-account

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { MainMenuPage } from '../pages/main-menu/main-menu';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    MainMenuPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +23,9 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    MainMenuPage
   ],
   providers: [
     StatusBar,
