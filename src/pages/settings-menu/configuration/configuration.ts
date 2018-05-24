29cf3425a02d2517a9a08ecf2770d9f92bdb019e
Jorge Michele Hernandez Bucci <jhernandez@virtualrobotstud.io>
2018-05-24 11:02:46 -0500

    formulario opciones inicializado, falta backend y estilo

--- src/pages/settings-menu/configuration/configuration.ts
+++ src/pages/settings-menu/configuration/configuration.ts
@@ -1,6 +1,7 @@
-import { Component } from "@angular/core";
-import { ViewController, PopoverController } from "ionic-angular";
+import { Component, OnInit } from "@angular/core";
+import { ViewController, PopoverController, Toggle } from "ionic-angular";
 import { SettingsMenuPage } from "../settings-menu";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
     template: `<ion-grid text-center>
@@ -9,49 +10,64 @@ import { SettingsMenuPage } from "../settings-menu";
                             <h2>Audio</h2>
                         </ion-col>
                     </ion-row>
-                    <ion-row>
-                        <ion-col>
-                            <ion-row>
-                                <ion-col>
-                                    Music
-                                </ion-col>                                
-                            </ion-row>
-                            <ion-row>
-                                <ion-col>
-                                    <ion-toggle checked="true"></ion-toggle>
-                                </ion-col>
-                            </ion-row>
-                        </ion-col>
-                        <ion-col>
-                            <ion-row>
-                                <ion-col>
-                                    SFX
-                                </ion-col>
-                            </ion-row>
-                            <ion-row>
-                                <ion-col >
-                                    <ion-toggle checked="true"></ion-toggle>
-                                </ion-col>
-                            </ion-row>
-                        </ion-col>
-                    </ion-row>
-                    <ion-row>
-                        <ion-col>
-                            <h2>Language</h2>
-                            <ion-select>
-                            <!--OJO ACA!!-->
-                                <ion-option
-                                    *ngFor="let lang of languages">
-                                    {{lang}}
-                                </ion-option>
-                            </ion-select>
-                        </ion-col>
-                    </ion-row>
-                    <ion-row>
-                        <ion-col>
-                            <h2>Notifications</h2>
-                        </ion-col>
-                    </ion-row>
+                    <form [formGroup]="configForm">
+                    
+                        <ion-row>
+                            <ion-col>
+                                <ion-row>
+                                    <ion-col>
+                                        Music
+                                    </ion-col>                                
+                                </ion-row>
+                                <ion-row>
+                                    <ion-col>
+                                        <ion-toggle 
+                                            formControlName="music"
+                                            checked="music"
+                                            (ionChange)="onToggle($event,'music')">
+                                        </ion-toggle>
+                                    </ion-col>
+                                </ion-row>
+                            </ion-col>
+                            <ion-col>
+                                <ion-row>
+                                    <ion-col>
+                                        SFX
+                                    </ion-col>
+                                </ion-row>
+                                <ion-row>
+                                    <ion-col >
+                                        <ion-toggle 
+                                        formControlName="sfx" 
+                                        checked="sfx"
+                                        (ionChange)="onToggle($event,'sfx')"></ion-toggle>
+                                    </ion-col>
+                                </ion-row>
+                            </ion-col>
+                        </ion-row>
+                        <ion-row>
+                            <ion-col>
+                                <h2>Language</h2>
+                                <ion-select formControlName="language" (ionChange)="onSelectChange($event)">
+                                <!--OJO ACA!!-->
+                                    <ion-option
+                                        *ngFor="let language of languages"
+                                        [value]="language">
+                                        {{language}}
+                                    </ion-option>
+                                </ion-select>
+                            </ion-col>
+                        </ion-row>
+                        <ion-row>
+                            <ion-col>
+                                <h2>Notifications</h2>
+                                <ion-checkbox 
+                                formControlName="notifications"
+                                (ionChange)="onToggle($event,'notifications')"></ion-checkbox>
+                            </ion-col>
+                        </ion-row>
+
+                    </form>
                     <ion-row>
                         <ion-col>
                             <button ion-button color="danger" (click)="onClickBack()">Back</button>
@@ -60,16 +76,49 @@ import { SettingsMenuPage } from "../settings-menu";
                 </ion-grid>`,
 })
 
-export class ConfigurationPage{
+export class ConfigurationPage implements OnInit{
 
     languages = ['English','Spanish'];
+    currentLang: string = 'Spanish';
+    music: boolean;
+    sfx: boolean;
+    notifications: boolean;
+
+    configForm: FormGroup;
 
     constructor(private viewCtrl: ViewController, private popoverCtrl: PopoverController  ){}
     
+    ngOnInit(): void {
+        this.initializeForm();
+    }
+
     onClickBack(){
         const popover = this.popoverCtrl.create(SettingsMenuPage);
         popover.present();
 
         this.viewCtrl.dismiss();
+    }
+
+    initializeForm(){
+        let music = true;
+        let sfx = false;
+        let language = 'Spanish';
+        let notifications = false;
+
+        this.configForm = new FormGroup({
+            'music': new FormControl(music, Validators.required),
+            'sfx': new FormControl(sfx, Validators.required),
+            'language': new FormControl(language, Validators.required),
+            'notifications': new FormControl(notifications, Validators.required)
+        });
+    }
+
+    onToggle(toggle: Toggle, option: string){
+        console.log(toggle);
+        console.log(option);       
+    }
+
+    onSelectChange(selectedValue: any) {
+        console.log('Selected', selectedValue);
     }
 }
---
+++ src/services/configuration.service.ts
@@ -0,0 +1,6 @@
+export class ConfigurationService{
+    music: boolean = false;
+    sfx: boolean= false;
+    language: string = 'English';
+    notifications: boolean = true;
+}
