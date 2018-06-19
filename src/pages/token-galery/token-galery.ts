227da5f5cf50869a5c0c46d98861459665d5f6a4
Jorge Michele Hernandez Bucci <jhernandez@virtualrobotstud.io>
2018-06-18 21:10:02 -0500

    esqueleto popUp selector de tokens; matriz mostrando elementos, header y footer, boton de cerrar, falta paginacion y estilo

--- src/pages/token-galery/token-galery.ts
+++ src/pages/token-galery/token-galery.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -7,12 +7,17 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'token-galery.html',
 })
 export class TokenGaleryPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  tokenGallery: any [];
+  constructor(private viewCtrl: ViewController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TokenGaleryPage');
+    this.tokenGallery = [1,2,3,4,5,6,7,8,9]
+  }
+
+  onClose(){
+    this.viewCtrl.dismiss();
   }
 
 }
