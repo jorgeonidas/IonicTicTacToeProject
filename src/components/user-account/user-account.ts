c527fc062931399c7723703f6fdc48f1f4dbe138
Jorge Michele Hernandez Bucci <jhernandez@virtualrobotstud.io>
2018-07-03 17:48:10 -0500

    minor bug fix cambio de ion-img a img debido a bug de ionic

--- src/components/user-account/user-account.ts
+++ src/components/user-account/user-account.ts
@@ -11,9 +11,18 @@ export class UserAccountComponent {
   coins: number;
   eolas: number;
 
+  coinIconUrl : string; 
+  eolaIconUrl: string;
+  
+
   constructor() {
     console.log('Hello UserAccountComponent Component');
-    this.profileImgUlr = 'assets/imgs//user.png'//Obviamente esto cargara luego de un servicio
+    this.profileImgUlr = 'assets/imgs/user.png'//Obviamente esto cargara luego de un servicio
+    this.coins = 9999;
+    this.eolas = 9999;
+
+    this.coinIconUrl = "assets/imgs/coins.png";
+    this.eolaIconUrl = "assets/imgs/eolas.png";
   }
 
 }
