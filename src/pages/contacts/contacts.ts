3ad8a69861c0d8e438391e15a1c089a56f1cebae
Jorge Michele Hernandez Bucci <jhernandez@virtualrobotstud.io>
2018-07-11 14:47:31 -0500

    mejoras de estilo, cambio de icono para el status actual del contacto

--- src/pages/contacts/contacts.ts
+++ src/pages/contacts/contacts.ts
@@ -10,17 +10,26 @@ export class ContactsPage {
 
   
   GameIvnPending: string= 'Game Invitation Pending';
-
+  /*TODO HACER MAS CONTACTOS PARA REFLEJAR OTROS ESTADOS */
   contactTest2 = {
     status: 2,
     ranking: 'assets/imgs/medal-icon.png',
-    nick: 'Kim',
+    nick: 'AveryLongNickname',
     msjNot: {type:'notification',
               msjReaded: true,
               msj: ""},
     action: 2
   }
 
+  contactTest3 = {
+    status: 3,
+    ranking: 'assets/imgs/medal-icon.png',
+    nick: 'Bobe',
+    msjNot: {type:'message',
+              msjReaded: true,
+              msj: "a very very long message from this chat keeps writing"},
+    action: 4
+  }
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     console.log(this.contactTest2);
