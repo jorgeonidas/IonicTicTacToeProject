d5a1692a735291cc2b57313d12e6b4be58f2bb20
Virtual Robot <master@virtualrobotstud.io>
2018-07-13 15:24:42 -0600

    statusBar.hide()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,6 +35,8 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       splashScreen.hide();
+      statusBar.isVisible = false;
+      statusBar.hide();
       //statusBar.styleDefault();
 
     });
