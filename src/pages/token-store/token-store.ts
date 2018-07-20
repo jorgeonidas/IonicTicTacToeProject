3a98e7fd040b73afd224dc11c908ea68ad256076
Jorge Michele Hernandez Bucci <jhernandez@virtualrobotstud.io>
2018-07-20 13:20:40 -0500

    iphone x padding bottom para token store

--- src/pages/token-store/token-store.ts
+++ src/pages/token-store/token-store.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { TokenService } from '../../services/tokenService';
+import { Platform } from 'ionic-angular';
+
 @IonicPage()
 @Component({
   selector: 'page-token-store',
@@ -11,14 +13,21 @@ export class TokenStorePage {
   currentToken: string;
   maxPerPage: number = 6;
   tokensUris: string[] = new Array(this.maxPerPage);
+  iphonex: boolean;
+  deviceHeight: number;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private tokenService: TokenService) {
+  constructor(platform: Platform,public navCtrl: NavController, public navParams: NavParams, private tokenService: TokenService) {
     this.currentToken = tokenService.getTokens()[0];
     for(let i = 0; i< this.tokensUris.length; i++){
       this.tokensUris[i] = tokenService.getTokens()[i];
     }
 
     console.log(this.tokensUris);
+
+    this.deviceHeight = platform.height();
+
+    this.iphonex = this.deviceHeight > 800;
+    console.log("device",this.deviceHeight, this.iphonex);
     
   }
 
