630235ba2d15095446071fcb7f76ba769890458d
Jorge Michele Hernandez Bucci <jhernandez@virtualrobotstud.io>
2018-07-23 14:13:20 -0500

    directivas de la token store para iphone x

--- src/pages/token-store/token-store.ts
+++ src/pages/token-store/token-store.ts
@@ -11,12 +11,26 @@ import { Platform } from 'ionic-angular';
 export class TokenStorePage {
 
   currentToken: string;
-  maxPerPage: number = 6;
-  tokensUris: string[] = new Array(this.maxPerPage);
+  maxPerPage: number = 6; //maximo de 6 por pagina
+  maxPerPageIphoneX: number = 9; //maximo de 9 por pagina
+  tokensUris: string[];
   iphonex: boolean;
   deviceHeight: number;
 
   constructor(platform: Platform,public navCtrl: NavController, public navParams: NavParams, private tokenService: TokenService) {
+    //obtener altura del telefono
+    this.deviceHeight = platform.height();
+    this.iphonex = this.deviceHeight > 800;
+    console.log("device",this.deviceHeight, this.iphonex);
+    //maximo de paginacion dependiendo del la altura del dispositivo
+    if(this.iphonex){
+      this.tokensUris = new Array(this.maxPerPageIphoneX);
+      console.log(this.tokensUris.length);
+    }else{
+      this.tokensUris = new Array(this.maxPerPage);
+      console.log(this.tokensUris.length);
+    }
+
     this.currentToken = tokenService.getTokens()[0];
     for(let i = 0; i< this.tokensUris.length; i++){
       this.tokensUris[i] = tokenService.getTokens()[i];
@@ -24,10 +38,9 @@ export class TokenStorePage {
 
     console.log(this.tokensUris);
 
-    this.deviceHeight = platform.height();
+    
 
-    this.iphonex = this.deviceHeight > 800;
-    console.log("device",this.deviceHeight, this.iphonex);
+    
     
   }
 
